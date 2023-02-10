241ffdb4950254605d98aa936deca02575bdf221
Надія Ткач <nadiya77723@gmail.com>
2023-02-10 14:02:48 +0200

    /

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Products from './components/Products/Products';
+
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Products />
+      
       </header>
     </div>
   )
