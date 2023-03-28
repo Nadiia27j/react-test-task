d13f11f6e158ebb9440c9a3a9190c78ca5995446
Надія Ткач <nadiya77723@gmail.com>
2023-03-28 15:55:02 +0300

    /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
     </div>
-  )
+  );
 }
+
+// око для паролю у формі_____________________
+
+// import { ImEyeBlocked } from "react-icons/im";
+// import { ImEye } from "react-icons/im";
+
+// const [show, setShow] = useState(false);
+// const handleShow = () => {
+//   setShow(!show);
+// };
+
+// <DivPass onClick={handleShow}>
+// {show ? <ImEye /> : <ImEyeBlocked />}
+// </DivPass>
